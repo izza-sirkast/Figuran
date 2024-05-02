3351a3ba9f274899357905cd540ac3fd83e6a793
izza-sirkast <dawlaizza405@gmail.com>
2024-05-03 00:10:14 +0700

    Creating a navbar with basic navigation functionallity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,46 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Gallery</div>
+    <div>Gallery
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam laborum quos itaque possimus, quaerat nihil quod nesciunt perferendis eum. Saepe eligendi fugit deleniti reiciendis neque. Alias excepturi necessitatibus ipsum adipisci.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit rem, autem enim ad eos, dignissimos ea vero deleniti quis asperiores dolor blanditiis alias dolorem provident iste, similique dicta facilis omnis?
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum neque totam repellendus nobis obcaecati fugiat deserunt culpa illum ut mollitia debitis, veritatis reiciendis sed. Enim eaque quos non laborum est?
+    </div>
   );
 }
--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,27 +1,46 @@
-import React from 'react'
+'use client'
+import {useState} from 'react'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { FaSearch } from "react-icons/fa";
 
 type Props = {}
 
 export default function Navbar({}: Props) {
+  const [isMenuDrop, setIsMenuDrop] = useState(false)
+  const pathName = usePathname()
+
+  function dropMenu(){
+    setIsMenuDrop(val => !val)
+  }
+
   return (
-    <nav className='bg-black flex sticky top-0 px-6 sm:px-10 lg:px-16 py-3 text-white justify-between items-center w-full max-w-full overflow-hidden'>
-        <div className='flex items-center w-26'>
-            <div className='flex-column mr-2 cursor-pointer hover:bg-slate-700 p-2 rounded-md hover:ease-in duration-300'>
-                <div className='border-t-2 border-white w-7'></div>
-                <div className='border-t-2 border-white w-7 mt-1'></div>
-                <div className='border-t-2 border-white w-7 mt-1'></div>
-            </div>
+    <nav className='sticky top-0 z-10'>
+      <div className='bg-black flex px-6 sm:px-10 lg:px-16 py-3 text-white justify-between items-center w-full max-w-full overflow-hidden'>
+          <div className='flex items-center md:w-26'>
+              <button onClick={dropMenu} className='flex-column mr-2 cursor-pointer hover:bg-slate-700 p-2 rounded-md hover:ease-in duration-300'>
+                  <div className='border-t-2 border-white w-7'></div>
+                  <div className='border-t-2 border-white w-7 mt-1'></div>
+                  <div className='border-t-2 border-white w-7 mt-1'></div>
+              </button>
 
-            <h1 className='text-xl'>FIGURAN</h1>
+              <h1 className='text-xl'>FIGURAN</h1>
+          </div>
+
+          <h1 className='text-xl'>Gallery</h1>
+
+          <div className='flex items-center'>
+              <input type="text" className='text-black rounded-md placeholder:text-sm px-2 md:w-40 w-24' placeholder='Search...' />
+              <FaSearch className='text-2xl ml-1 text-black bg-white rounded-md p-1 cursor-pointer hover:bg-slate-400 hover:ease-in transition duration-150'/>
+          </div>
+      </div>
+
+      {isMenuDrop &&
+        <div className='flex flex-col justify-center items-center bg-black text-white border-t border-white pt-3 *:mb-2 absolute w-full'>
+          <Link href={'/'} className={`${pathName == '/' ? 'underline' : 'hover:text-slate-400'} `}>Gallery</Link>
+          <Link href={'/album'} className={`${pathName == '/album' ? 'underline' : 'hover:text-slate-400'}`}>Album</Link>
         </div>
-
-        <h1 className='text-xl'>Gallery</h1>
-
-        <div className='hidden md:flex items-center'>
-            <input type="text" className='text-black rounded-md placeholder:text-sm px-2 w-40' placeholder='Search...' />
-            <FaSearch className='text-2xl ml-1 text-black bg-white rounded-md p-1 cursor-pointer hover:bg-slate-400 hover:ease-in transition duration-150'/>
-        </div>
+      }
     </nav>
   )
 }
